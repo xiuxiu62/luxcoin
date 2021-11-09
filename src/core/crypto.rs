c70ea0c0e419d148bea035b06b3b971028f79d30
Justin Cremer <jacremer@live.com>
2021-11-08 17:01:33 -0800

    roll merkle tree into a struct and a From<&Vec<Transaction>> implementation

--- src/core/crypto.rs
+++ src/core/crypto.rs
@@ -6,12 +6,18 @@ use serde::{
 };
 use sha2::Digest;
 
+use crate::core::{block::BlockHash, transaction::Transaction};
+
 #[derive(Copy, Clone, Debug, Hash, PartialEq, Eq, PartialOrd, Ord)]
 pub struct Sha256([u8; 32]);
 
 impl Sha256 {
     pub const fn new(bytes: [u8; 32]) -> Self {
         Self(bytes)
+    }
+
+    pub fn as_slice(&self) -> &[u8] {
+        &self.0
     }
 }
 
@@ -127,33 +133,70 @@ pub fn hash(data: &[u8]) -> Sha256 {
     Sha256::new(output)
 }
 
-pub fn merkle_tree(leaves: &Vec<&[u8]>) -> MerkleHash {
-    let mut hashes: Vec<Sha256> = leaves.iter().map(|leaf| hash(*leaf)).collect();
-    while hashes.len() != 1 {
-        if hashes.len() % 2 == 1 {
-            hashes.push(hashes.last().unwrap().clone());
-        }
+pub fn target_hash(n_zero_bits: u32) -> BlockHash {
+    let mut hash = [0xff; 32];
 
-        let mut next_level_hashes = vec![];
-        (0..hashes.len()).step_by(2).for_each(|i: usize| {
-            let lhs = hashes.get(i).unwrap();
-            let rhs = hashes.get(i + 1).unwrap();
-
-            let mut concat: Vec<u8> = lhs.as_ref().iter().map(|byte: &u8| *byte).collect();
-            concat.extend_from_slice(rhs.as_ref());
-            next_level_hashes.push(hash(&concat));
-        });
-
-        hashes = next_level_hashes;
+    let num_zero_bytes = (n_zero_bits / 8) as usize;
+    for i in 0..num_zero_bytes {
+        hash[i] = 0;
     }
 
-    MerkleHash::new(hashes.into_iter().next().unwrap())
+    let remainder = 8 - (n_zero_bits % 8);
+    if remainder == 8 {
+        return BlockHash::new(Sha256::new(hash));
+    }
+
+    hash[num_zero_bytes] = (1 << remainder) - 1;
+    BlockHash::new(Sha256::new(hash))
+}
+
+pub struct MerkleTree(MerkleHash);
+
+impl MerkleTree {
+    pub fn new(leaves: &Vec<&[u8]>) -> MerkleTree {
+        let mut hashes: Vec<Sha256> = leaves.iter().map(|leaf| hash(*leaf)).collect();
+        while hashes.len() != 1 {
+            if hashes.len() % 2 == 1 {
+                hashes.push(hashes.last().unwrap().clone());
+            }
+
+            let mut next_level_hashes = vec![];
+            (0..hashes.len()).step_by(2).for_each(|i: usize| {
+                let lhs = hashes.get(i).unwrap();
+                let rhs = hashes.get(i + 1).unwrap();
+
+                let mut concat: Vec<u8> = lhs.as_ref().iter().map(|byte: &u8| *byte).collect();
+                concat.extend_from_slice(rhs.as_ref());
+                next_level_hashes.push(hash(&concat));
+            });
+
+            hashes = next_level_hashes;
+        }
+
+        MerkleTree(MerkleHash::new(hashes.into_iter().next().unwrap()))
+    }
+}
+
+impl AsRef<MerkleHash> for MerkleTree {
+    fn as_ref(&self) -> &MerkleHash {
+        &self.0
+    }
+}
+
+impl From<&Vec<Transaction>> for MerkleTree {
+    fn from(transactions: &Vec<Transaction>) -> Self {
+        let leaves = transactions
+            .iter()
+            .map(|tx| tx.id().as_ref().as_slice())
+            .collect::<Vec<&[u8]>>();
+        MerkleTree::new(&leaves)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{hash, merkle_tree};
-    use crate::core::crypto::as_hex;
+    use super::hash;
+    use crate::core::crypto::{as_hex, target_hash, MerkleTree};
 
     #[test]
     fn hash_works() {
@@ -165,16 +208,16 @@ mod tests {
     }
     #[test]
     fn merkle_tree_even() {
-        let root_node = merkle_tree(&vec![b"hello", b"world"]);
+        let root_node = MerkleTree::new(&vec![b"hello", b"world"]);
         assert_eq!(
-            as_hex(root_node.as_slice()),
+            as_hex(root_node.as_ref().as_slice()),
             "7305db9b2abccd706c256db3d97e5ff48d677cfe4d3a5904afb7da0e3950e1e2"
         );
     }
 
     #[test]
     fn merkle_tree_odd() {
-        let root_node = merkle_tree(&vec![
+        let root_node = MerkleTree::new(&vec![
             b"programmed",
             b"to",
             b"work",
@@ -184,14 +227,14 @@ mod tests {
             b"feel",
         ]);
         assert_eq!(
-            as_hex(root_node.as_slice()),
+            as_hex(root_node.as_ref().as_slice()),
             "4ba2b808c60bdee5df9da358021b50ae56f544682c7931fcc032d2ca323c13bb"
         )
     }
 
     #[test]
     fn merkle_tree_even_same_as_previous() {
-        let root_node = merkle_tree(&vec![
+        let root_node = MerkleTree::new(&vec![
             b"programmed",
             b"to",
             b"work",
@@ -202,8 +245,36 @@ mod tests {
             b"feel",
         ]);
         assert_eq!(
-            as_hex(root_node.as_slice()),
+            as_hex(root_node.as_ref().as_slice()),
             "4ba2b808c60bdee5df9da358021b50ae56f544682c7931fcc032d2ca323c13bb"
         )
+    }
+
+    #[test]
+    fn target_hash_test() {
+        assert_eq!(
+            as_hex(target_hash(0).as_slice()),
+            "ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+        assert_eq!(
+            as_hex(target_hash(4).as_slice()),
+            "0fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+        assert_eq!(
+            as_hex(target_hash(8).as_slice()),
+            "00ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+        assert_eq!(
+            as_hex(target_hash(12).as_slice()),
+            "000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+        assert_eq!(
+            as_hex(target_hash(16).as_slice()),
+            "0000ffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
+        assert_eq!(
+            as_hex(target_hash(20).as_slice()),
+            "00000fffffffffffffffffffffffffffffffffffffffffffffffffffffffffff"
+        );
     }
 }
